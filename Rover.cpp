601c6ee842aad44570ea2ebdc4df66ee813278d8
Kevin <kevin.baza@hotmail.com>
2017-02-19 15:11:19 -0500

    Added rover cpp file containing Arduino lib.

---
+++ Rover.cpp
@@ -0,0 +1,37 @@
+#include "Rover.h"
+#include "Arduino.h"
+
+
+Rover::Rover(int pin){
+
+    for(boardPins = 3; boardPins < 13; boardPins ++){
+        digitalWrite(boardPins, LOW);
+        pinMode(boardPins, OUTPUT);
+    }
+
+    sensorPin = pin;
+}
+
+
+int Rover::irDistance(){                    //Calculate distance.
+
+    int distance = 0;
+    long ADC_result;
+
+    if(sensorPin < 0 || sensorPin > 5){
+        Serial.println("Wrong distance pin");
+        return -1;
+    }
+
+    ADC_result = analogRead(sensorPin);
+    ADC_result = ADC_result * 500/1023;     //May be wrong. DEBUG. adc -> v
+
+    if(ADC_result == 0){
+        ADC_result = 1;                     //Test.
+    }
+
+    ADC_result = 1280/ADC_result - 1;       //Distance formula.
+    distance = (int)ADC_result;
+
+    return distance;
+}
