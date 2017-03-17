f642588ee56a9094cba3c96fd55ad9fe4bab07e6
Cap-n-Cook <kbaza001@fiu.edu>
2017-03-16 21:20:06 -0400

    Added test file

---
+++ test_image.py
@@ -0,0 +1,20 @@
+#import necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+#init the camera and grab a reference to a raw cam capture
+camera = PiCapture()
+rawCapture = PiRGBArray(camera)
+
+#camera warmup
+time.sleep(0.1)
+
+#grab image from camera
+camera.capture(rawCapture, format = "bgr")
+image = rawCapture.array
+
+#display image on screen and wait for a KeyboardInterrupt
+cv2.imshow("Image", image)
+cv2.waitKey(0)
